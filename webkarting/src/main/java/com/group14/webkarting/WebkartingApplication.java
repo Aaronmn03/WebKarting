8503ff76ac9aa213dfb9c60a4e8f45b66463db60
Aaron <aaron.mn03@gmail.com>
2024-03-13 11:20:05 +0100

    Proyecto creado

---
+++ webkarting/src/main/java/com/group14/webkarting/WebkartingApplication.java
@@ -0,0 +1,13 @@
+package com.group14.webkarting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebkartingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebkartingApplication.class, args);
+	}
+
+}
---
+++ webkarting/src/test/java/com/group14/webkarting/WebkartingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group14.webkarting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebkartingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
