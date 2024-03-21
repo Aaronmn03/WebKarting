e4c515fe98026f6a04b404eb071fe6786ca9ef4f
Aaron <aaron.mn03@gmail.com>
2024-03-21 12:02:49 +0100

    Race implemented

--- webkarting/src/main/java/com/group14/webkarting/Controllers/RaceController.java
+++ webkarting/src/main/java/com/group14/webkarting/Controllers/RaceController.java
@@ -3,6 +3,7 @@ package com.group14.webkarting.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.group14.webkarting.Services.RaceService;
@@ -20,5 +21,17 @@ public class RaceController {
     @RequestMapping("/newRace")
     public String newRace(Model model) {
         return "new_race";
+    }
+
+    @RequestMapping("/Race/{id}/")
+    public String getReserve(Model model,@PathVariable long id) {
+        model.addAttribute("race", raceService.findById(id));
+        return "race";
+    }
+
+    @RequestMapping("/Race/{id}/User/{idUser}")
+    public String getReserve(Model model,@PathVariable long id, @PathVariable long idUser) {
+        model.addAttribute("driver", raceService.findById(id).getListDrivers().get((int) idUser));
+        return "raceUser";
     }
 }
--- webkarting/src/main/java/com/group14/webkarting/Models/Race.java
+++ webkarting/src/main/java/com/group14/webkarting/Models/Race.java
@@ -1,7 +1,5 @@
 package com.group14.webkarting.Models;
 
-import java.time.LocalTime;
-import java.time.LocalDate;
 import java.util.List;
 
 import com.group14.webkarting.utils.Driver;
@@ -9,15 +7,11 @@ import com.group14.webkarting.utils.Driver;
 public class Race {
     private Long id;
     private List<Driver> listDrivers;
-    private LocalDate date;
-    private LocalTime startRace;
-    private LocalTime endRace;
+    private int numberLaps;
 
-    public Race(List<Driver> listDrivers, LocalDate date, LocalTime startRace, LocalTime endRace){
+    public Race(List<Driver> listDrivers,int numberLaps){
         this.listDrivers = listDrivers;
-        this.date = date;
-        this.startRace = startRace;
-        this.endRace = endRace;
+        this.numberLaps = numberLaps;
     }
 
     public Long getId() {
@@ -36,29 +30,12 @@ public class Race {
         this.listDrivers = listDrivers;
     }
 
-    public LocalDate getDate() {
-        return date;
+    public int getNumberLaps() {
+        return numberLaps;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
+    public void setNumberLaps(int numberLaps) {
+        this.numberLaps = numberLaps;
     }
-
-    public LocalTime getStartRace() {
-        return startRace;
-    }
-
-    public void setStartRace(LocalTime startRace) {
-        this.startRace = startRace;
-    }
-
-    public LocalTime getEndRace() {
-        return endRace;
-    }
-
-    public void setEndRace(LocalTime endRace) {
-        this.endRace = endRace;
-    }
-
     
 }
