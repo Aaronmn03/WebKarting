4596ea282fd1a6e0433005f1b2e5d4112201ffe5
Juanjo2633 <juanjoguirao2633@gmail.com>
2024-04-02 14:28:56 +0200

    Cors fixed

--- webkarting/src/main/java/com/group14/webkarting/WebkartingApplication.java
+++ webkarting/src/main/java/com/group14/webkarting/WebkartingApplication.java
@@ -2,11 +2,27 @@ package com.group14.webkarting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WebkartingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebkartingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebkartingApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("Content-Type", "Authorization");
+            }
+        };
+    }
 }
